e625587d580079d4f39d8e545f3f9158dc5a3f73
blunden <blunden2@gmail.com>
2018-03-18 14:02:30 +0100

    Add setting to enable Do Not Disturb during calls
    Android N and earlier enabled Do Not Disturb mode while in a call.
    Reimplement this behavior to prevent incoming notifications from
    vibrating or playing sounds.
    
    Change-Id: Ic38ac775c6e353898190e1571b6d6e521c0e5aa1

--- java/com/android/dialer/app/settings/OtherSettingsFragment.java
+++ java/com/android/dialer/app/settings/OtherSettingsFragment.java
@@ -16,7 +16,11 @@
 
 package com.android.dialer.app.settings;
 
+import android.app.AlertDialog;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -36,6 +40,9 @@ public class OtherSettingsFragment extends PreferenceFragment
   private boolean mEnabled;
 
   private SwitchPreference mEnablePostcall;
+  private SwitchPreference enableDndInCall;
+
+  private NotificationManager notificationManager;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -58,6 +65,10 @@ public class OtherSettingsFragment extends PreferenceFragment
         context.getString(R.string.incall_vibration_category_key));
     ps.removePreference(inCallVibration);
 
+    enableDndInCall = (SwitchPreference) findPreference("incall_enable_dnd");
+    enableDndInCall.setOnPreferenceChangeListener(this);
+    notificationManager = context.getSystemService(NotificationManager.class);
+
   }
 
   @Override
@@ -69,6 +80,31 @@ public class OtherSettingsFragment extends PreferenceFragment
           .putBoolean(ENABLE_POST_CALL, value)
           .apply();
         return true;
+    } else if (preference == enableDndInCall) {
+      boolean newValue = (Boolean) objValue;
+      if (newValue && !notificationManager.isNotificationPolicyAccessGranted()) {
+        new AlertDialog.Builder(getContext())
+            .setMessage(R.string.incall_dnd_dialog_message)
+            .setPositiveButton(R.string.allow, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                startActivity(intent);
+              }
+            })
+            .setNegativeButton(R.string.deny, new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+              }
+            })
+            .show();
+
+        // At this time, it is unknown whether the user granted the permission
+        return false;
+      }
+      return true;
     }
     return false;
   }
