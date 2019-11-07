b1bb49b6a1df381415fd61b5023d9cb7a221df97
Danny Baumann <dannybaumann@web.de>
2019-11-07 08:34:44 +0100

    Base 'call recording allowed' decision on current country.
    Selection of resources by MCC happens via the SIM MCC, but what matters
    for legislation is the current country, not the country the SIM origins
    from. Because of that, move the decision about whether call recording is
    allowed or not to the current country instead of SIM MCC.
    
    Change-Id: I0ee365d7af8e3392716318e5a51e12e0efe7029a

--- java/com/android/incallui/CallButtonPresenter.java
+++ java/com/android/incallui/CallButtonPresenter.java
@@ -575,7 +575,7 @@ public class CallButtonPresenter
             && call.getState() != DialerCallState.CONNECTING;
 
     final CallRecorder recorder = CallRecorder.getInstance();
-    final boolean showCallRecordOption = recorder.isEnabled()
+    final boolean showCallRecordOption = recorder.canRecordInCurrentCountry()
         && !isVideo && call.getState() == DialerCallState.ACTIVE;
 
     otherAccount = TelecomUtil.getOtherAccount(getContext(), call.getAccountHandle());
